7af28fb1f94faf3f7a417c1fa93e38cf642dada5
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-28 14:14:20 -0600

    added app.get

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const app = express()
 
 const port = process.env.PORT || 5150
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/index.html'))
+    rollbar.info('Html was monitored successfully!')
+})
+
 app.listen(port, () => console.log(`Listening in the halls on port: ${port}`))
