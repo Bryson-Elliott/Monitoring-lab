5dcdb9ed88bdff9a8ff9329e400170b63ff3a302
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-28 14:18:08 -0600

    remembering to forget

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
-const port = process.env.PORT || 5150
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './client/index.html'))
     //rollbar.info('Html was monitored successfully!')
 })
+
+const port = process.env.PORT || 5150
 
 app.listen(port, () => console.log(`Listening in the halls on port: ${port}`))
